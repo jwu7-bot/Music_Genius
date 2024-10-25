d0bd0fe16e1809da587bceb5171c5740782af44f
JiaHui Wu <jwu7@rrc.ca>
2024-10-25 17:12:05 -0500

    Implemented the Artist controller index to use pagination and show.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,11 @@
 class ArtistsController < ApplicationController
   def index
+    # fetch artists
+    @artists = Artist.order(:name).paginate(page: params[:page], per_page: 25)
   end
 
   def show
+    # fecth individual artists
+    @artist = Artist.find(params[:id])
   end
 end
