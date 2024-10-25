a8201a2bab06219cedbd7e90d9596f23542bafca
JiaHui Wu <jwu7@rrc.ca>
2024-10-25 17:19:18 -0500

    Implemented the Track controller index using pagination and show.

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,7 +1,11 @@
 class TracksController < ApplicationController
   def index
+    # fetch all tracks
+    @tracks = Track.includes(:artist, :genre).order(:name).paginate(page: params[:page], per_page: 25)
   end
 
   def show
+    # fetch individual tracks
+    @track = Track.find(params[:id])
   end
 end
