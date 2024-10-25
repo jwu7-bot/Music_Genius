044f5d3ce24b27213c27cf172c56fc7f41dd30dd
JiaHui Wu <jwu7@rrc.ca>
2024-10-25 17:14:10 -0500

    Generated the Genre controller with index and show views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "genres/index"
+  get "genres/show"
   get "artists/index"
   get "artists/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
