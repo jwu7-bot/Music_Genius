e945a6d1f375e11bc75a6a5dbd6288208c3485a0
JiaHui Wu <jwu7@rrc.ca>
2024-10-25 19:56:37 -0500

    Updated the track controller and index view in order to display all the genres that the track is from.

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,11 +1,11 @@
 class TracksController < ApplicationController
   def index
     # fetch all tracks
-    @tracks = Track.includes(:artist, :genre).order(:name).paginate(page: params[:page], per_page: 25)
+    @tracks = Track.includes(:artist).order(:name).paginate(page: params[:page], per_page: 25)
   end
 
   def show
     # fetch individual tracks
-    @track = Track.find(params[:id])
+    @track = Track.includes(:genres).find(params[:id])
   end
 end
